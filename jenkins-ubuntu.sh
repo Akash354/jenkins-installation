3f75ce6ee2c32acb482d934c5c52d98e7d4989a8
Akash354 <59053084+Akash354@users.noreply.github.com>
2023-08-06 13:20:55 +0530

    Add files via upload
    shell script for installing jenkins on ubuntu

---
+++ jenkins-ubuntu.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+apt-get update
+apt install -y openjdk-17-jre-headless
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+apt update
+apt install -y jenkins
+systemctl start jenkins
+systemctl status jenkins
